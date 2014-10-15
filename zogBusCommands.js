9b990d4e912f88959674cdd0be4a6b0a0a440449
Samuel Loup <samuel.loup@gmail.com>
2014-10-15 13:32:48 +0200

    new way to call bus commands (with new bus commands api)

--- zogBusCommands.js
+++ zogBusCommands.js
@@ -1,14 +1,18 @@
 'use strict';
 /* xcraft-zog busclient shell extensions */
-
+var xLog   = require ('xcraft-core-log')('zog-buscmdloader');
 exports.load = function (settings) {
   if (settings.shell !== undefined && settings.busClient !== undefined) {
     var commander = require ('xcraft-core-bus').getCommander();
     var registry  = commander.getCommandsRegistry ();
     Object.keys (registry).forEach (function (action) {
-      settings.shell.cmd (action, registry[action].desc, function (req, res, next) {
-        settings.busClient.command.send (action, null, null);
-        return res.prompt();
+      var cmdAndParams = action;
+      if (registry[action].params) {
+        cmdAndParams += ' ' + registry[action].params;
+      }
+      
+      settings.shell.cmd (cmdAndParams, registry[action].desc, function (req, res, next) {
+        settings.busClient.command.send (action, req.params, function () { settings.shell.set('stdout', process.stdout); res.prompt(); });
       });
     });
   }
