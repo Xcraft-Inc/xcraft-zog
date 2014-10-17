884003bef7dc8c415a5cbf1d57680bcffbf87eda
Mathieu Schroeter <schroeter@epsitec.ch>
2014-10-17 14:54:56 +0200

    Cosmetics.

--- busServer.js
+++ busServer.js
@@ -1,27 +1,27 @@
 'use strict';
+
+var zogBoot = require ('./zogBoot.js');
 var zogLog  = require ('xcraft-core-log') ('bus-server');
-zogLog.verbosity (1);
-var zogBoot      = require ('./zogBoot.js');
-
-
+zogLog.verbosity (0);
 
 
 var server = function () {
+  /* Subscribe to the shutdown event, then call mainShutdown. */
   var mainShutdown = function () {
     zogLog.verb ('shutdown...');
     zogBoot.stop ();
   };
 
-  zogBoot.bus.getCommander().registerAutoconnectHandler(function () {
-    var commander = require ('xcraft-core-bus').getCommander();
+  zogBoot.bus.getCommander().registerAutoconnectHandler (function () {
+    var commander = require ('xcraft-core-bus').getCommander ();
     var registry  = commander.getCommandsRegistry ();
     var connectedMsg = {
-      token : zogBoot.busClient.getToken(),
+      token       : zogBoot.busClient.getToken(),
       cmdRegistry : registry
     };
+
     zogBoot.busClient.events.send ('connected', connectedMsg);
   });
 };
-
 
 zogBoot.start (server);
