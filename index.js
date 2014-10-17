f70398c3fc700f1c9a98f07217b1bbad79aac20c
Mathieu Schroeter <schroeter@epsitec.ch>
2014-10-17 15:24:13 +0200

    Use xcraft-core-process in order to fork.

--- index.js
+++ index.js
@@ -35,23 +35,8 @@ var shellStart = function (busClient) {
 
 var serverStart = function () {
   var args = [];
-  var server = require ('child_process').fork (path.join (__dirname, './busServer.js'), args, {silent: true});
-
-  server.stdout.on ('data', function (data) {
-    data.toString ().replace (/\r/g, '').split ('\n').forEach (function (line) {
-      if (line.trim ().length) {
-        console.log ('BUS-SERVER: ' + line);
-      }
-    });
-  });
-
-  server.stderr.on ('data', function (data) {
-    data.toString ().replace (/\r/g, '').split ('\n').forEach (function (line) {
-      if (line.trim ().length) {
-        console.log ('BUS-SERVER: ' + line);
-      }
-    });
-  });
+  var xProcess = require ('xcraft-core-process');
+  xProcess.fork (path.join (__dirname, './busServer.js'), args, {silent: true});
 
   var busClient = require ('xcraft-core-busclient');
   busClient.connect (null, function (err) {
