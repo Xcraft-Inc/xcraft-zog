6b768f6618de0b6cb0f2c9a2c86dae734d45b20c
Mathieu Schroeter <schroeter@epsitec.ch>
2014-10-17 15:10:05 +0200

    The zogShell must start the busServer.

--- index.js
+++ index.js
@@ -2,18 +2,61 @@
 
 var inquirer = require ('inquirer');
 var async    = require ('async');
+var path     = require ('path');
+var xLog     = require ('xcraft-core-log') ('zog');
+xLog.verbosity (0);
 
-var shell = [{
-  type    : 'input',
-  name    : 'command',
-  message : 'z0g>'
-}];
 
-async.forever (function (next) {
-  inquirer.prompt (shell, function (answers) {
-    // TODO
-    next();
+var shellStart = function (busClient) {
+  var shell = [{
+    type    : 'input',
+    name    : 'command',
+    message : 'z0g>'
+  }];
+
+  var mainShutdown = function () {
+    busClient.stop (function (done) { /* jshint ignore:line */
+      xLog.verb ('bus client stopped...');
+    });
+  };
+
+  async.forever (function (next) {
+    inquirer.prompt (shell, function (answers) {
+      next ();
+    });
+  }, function (err) {
+    if (err) {
+      xLog.err (err);
+    }
+
+    mainShutdown ();
   });
-}, function (err) {
-  console.log (err);
-});
+};
+
+var serverStart = function () {
+  var args = [];
+  var server = require ('child_process').fork (path.join (__dirname, './busServer.js'), args, {silent: true});
+
+  server.stdout.on ('data', function (data) {
+    data.toString ().replace (/\r/g, '').split ('\n').forEach (function (line) {
+      if (line.trim ().length) {
+        xLog.verb ('BUS-SERVER: ' + line);
+      }
+    });
+  });
+
+  server.stderr.on ('data', function (data) {
+    data.toString ().replace (/\r/g, '').split ('\n').forEach (function (line) {
+      if (line.trim ().length) {
+        xLog.verb ('BUS-SERVER: ' + line);
+      }
+    });
+  });
+
+  var busClient = require ('xcraft-core-busclient');
+  busClient.connect (null, function (err) {
+    shellStart (busClient);
+  });
+};
+
+serverStart ();
