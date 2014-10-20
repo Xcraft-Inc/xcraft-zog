3c3bf902b9213de054820d3b1ee7e97d7abfdc25
Mathieu Schroeter <schroeter@epsitec.ch>
2014-10-20 08:48:32 +0200

    Factorize.

--- busServer.js
+++ busServer.js
@@ -6,15 +6,15 @@ zogLog.verbosity (0);
 
 
 var server = function () {
+  var commander = zogBoot.bus.getCommander();
 
-  zogBoot.bus.getCommander().registerShutdownHandler (function () {
+  commander.registerShutdownHandler (function () {
     zogBoot.busClient.events.send ('disconnected');
     zogLog.verb ('shutdown...');
     zogBoot.stop ();
   });
 
-  zogBoot.bus.getCommander().registerAutoconnectHandler (function () {
-    var commander = require ('xcraft-core-bus').getCommander ();
+  commander.registerAutoconnectHandler (function () {
     var registry  = commander.getCommandsRegistry ();
     var connectedMsg = {
       token       : zogBoot.busClient.getToken(),
