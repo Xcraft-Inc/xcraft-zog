d66f7edb4acda23a6ba8978860bea5cd0d8db80d
Mathieu Schroeter <schroeter@epsitec.ch>
2014-10-30 14:31:55 +0100

    Major rework of xcraft-zog shell. It's now using the shellcraft node module. Note that the inquirer part for the chest server (upload by pacman) is broken.

---
+++ extensions.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var xServer = require ('xcraft-core-server');
+xServer.fork (null, function (line) {
+  console.log ('server ' + process.pid + ': ' + line);
+}, function (line) {
+  console.log ('server ' + process.pid + ': ' + line);
+});
+
+var busClient = require ('xcraft-core-busclient');
+
+exports.register = function (callback) {
+  busClient.connect (null, function (err) {
+    var commands = [];
+
+    var mainShutdown = function () {
+      busClient.stop (function (done) { /* jshint ignore:line */
+      });
+    };
+
+    busClient.events.subscribe ('disconnected', function (msg) { /* jshint ignore:line */
+      mainShutdown ();
+    });
+
+    var cmdList = busClient.getCommandsRegistry ();
+    Object.keys (cmdList).forEach (function (cmd) {
+      commands.push ({
+        name    : cmd,
+        desc    : cmdList[cmd].desc,
+        options : cmdList[cmd].options || {},
+        handler : function (callback, args) {
+          var params = {};
+
+          if (cmdList[cmd].params) {
+            params[cmdList[cmd].params] = args[0];
+          }
+
+          busClient.events.subscribe (cmdList[cmd].name + '.finished', function () {
+            callback ();
+          });
+
+          busClient.events.subscribe (cmdList[cmd].name + '.added', function (msg) {
+            var wizard = require (msg.data.wizardPath);
+
+            var i = 0;
+            wizard[msg.data.wizardName].forEach (function (input) {
+              Object.keys (msg.data.wizardDefaults).some (function (name) {
+                if (input.name === name) {
+                  wizard[msg.data.wizardName][i].default = msg.data.wizardDefaults[name];
+                  return true;
+                }
+                return false;
+              });
+              ++i;
+            });
+
+            callback (wizard[msg.data.wizardName], function (answers) {
+              msg.data.wizardAnswers.push (answers);
+              if (msg.data.nextCommand) {
+                busClient.command.send (msg.data.nextCommand, msg.data);
+              }
+              return false;
+            });
+          });
+
+          busClient.command.send (cmdList[cmd].name, params);
+        }
+      });
+    });
+
+    callback (commands);
+  });
+};
+
+exports.unregister = function () {
+  busClient.command.send ('shutdown');
+};
--- index.js
+++ index.js
@@ -1,166 +1,17 @@
 'use strict';
 
-var inquirer = require ('inquirer');
-var program  = require ('commander');
-var async    = require ('async');
-var xLog     = require ('xcraft-core-log') ('zog');
-xLog.verbosity (0); /* FIXME: make a builtin command */
+var path       = require ('path');
+var shellcraft = require ('shellcraft');
 
-/* TODO: we must use commander in order to handle the non-shell side of
- *       zogShell.
- */
+var zogLog = require ('xcraft-core-log') ('bus-server');
+zogLog.verbosity (0);
 
-var shellCommands = function (cmdList, busClient) {
-  /* Builtin shell commands */
-  var list = {
-    help: {
-      params  : null,
-      desc    : 'list of commands',
-      handler : null
-    },
-    exit: {
-      params  : null,
-      desc    : 'exit zogShell',
-      handler : null
-    }
-  };
-
-  Object.keys (cmdList).forEach (function (cmd) {
-    list[cmd] = {
-      params  : cmdList[cmd].params,
-      desc    : cmdList[cmd].desc,
-      handler : function (arg) {
-        var args = {};
-
-        if (cmdList[cmd].params) {
-          args[cmdList[cmd].params] = arg;
-        }
-        /* TODO: finishHandler */
-        busClient.command.send (cmdList[cmd].name, args, null);
-      }
-    };
-  });
-
-  return list;
+var options = {
+  prompt: 'z0g>'
 };
 
-var shellStart = function (busClient) {
-  var shell = [{
-    type    : 'input',
-    name    : 'command',
-    message : 'z0g>'
-  }];
-
-  var cmdList = busClient.getCommandsRegistry ();
-  var shellCmdList = shellCommands (cmdList, busClient);
-
-  shellCmdList.help.handler = function () {
-    Object.keys (shellCmdList).forEach (function (cmd) {
-      /* TODO: show desc and params */
-      console.log (cmd);
-    });
-  };
-
-  var exitShell = false;
-  shellCmdList.exit.handler = function () {
-    exitShell = true;
-  };
-
-  async.forever (function (next) {
-    inquirer.prompt (shell, function (answers) {
-      var cmd = answers.command.split (' ');
-
-      try {
-        shellCmdList[cmd[0]].handler (cmd[1]);
-      } catch (ex) {
-        if (answers.command.length) {
-          console.log ('command ' + cmd[0] + ' unknown');
-        }
-      }
-      next (exitShell ? 'good bye' : null);
-    });
-  }, function (err) {
-    if (err) {
-      xLog.info (err);
-    }
-
-    busClient.command.send ('shutdown');
+shellcraft.registerExtension (path.join (__dirname, 'extensions.js'), function () {
+  shellcraft.begin (options, function (msg) {
+    console.log (msg);
   });
-};
-
-var serverStart = function () {
-  var xServer = require ('xcraft-core-server');
-  xServer.fork (null, function (line) {
-    console.log ('server: ' + line);
-  }, function (line) {
-    console.log ('server: ' + line);
-  });
-
-  var busClient = require ('xcraft-core-busclient');
-
-  busClient.connect (null, function (err) {
-    var mainShutdown = function () {
-      busClient.stop (function (done) { /* jshint ignore:line */
-        xLog.verb ('bus client stopped...');
-      });
-    };
-
-    var shellExecute = function (command, args) {
-      busClient.command.send (command, args);
-      busClient.command.send ('shutdown');
-    };
-
-    busClient.events.subscribe ('disconnected', function (msg) {
-      mainShutdown ();
-    });
-
-    program
-      .version ('0.1.0')
-      .option ('-v, --verbosity <level>', 'change the verbosity level [0..3] (default: 1)', xLog.verbosity)
-      .option ('-n, --nocolor', 'disable the color output\n');
-
-    var cmdList = busClient.getCommandsRegistry ();
-    Object.keys (cmdList).forEach (function (cmd) {
-      var cmdInquirer = cmd;
-
-      if (cmdList[cmd].params) {
-        cmdInquirer += ' <' + cmdList[cmd].params + '>';
-      }
-
-      program.option (cmdInquirer, cmdList[cmd].desc, function (arg) {
-        var args = {};
-
-        if (cmdList[cmd].params) {
-          args[cmdList[cmd].params] = arg;
-        }
-
-        shellExecute (cmd, args);
-      });
-    });
-
-    program.on ('--help', function () {
-      console.log ('  Informations:');
-      console.log ('');
-      console.log ('    Please be careful when using `zog clean` because the installed packages');
-      console.log ('    are not removed properly. For example, if a MSI was installed by a package,');
-      console.log ('    it will remains in the system. The reason is that only the devroot/ is');
-      console.log ('    deleted regardless of wpkg.');
-      console.log ('');
-      console.log ('  Examples:');
-      console.log ('');
-      console.log ('    $ zog lokthar install');
-      console.log ('    $ zog -n lokthar run');
-      console.log ('    $ zog edit libfoobar');
-      console.log ('    $ zog -v 0 make');
-      console.log ('');
-    });
-
-    program.parse (process.argv);
-
-    if (process.argv.length === 2) {
-      shellStart (busClient);
-    }
-  });
-};
-
-serverStart ();
+});
